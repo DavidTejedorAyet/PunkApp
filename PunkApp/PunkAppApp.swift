425e0d5e6cfec7940e8da1df3459ca97260ffba9
David Tejedor <d.tejedor@proun.es>
2021-10-07 09:54:50 +0200

    Initial Commit

---
+++ PunkApp/PunkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunkAppApp.swift
+//  PunkApp
+//
+//  Created by David on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct PunkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
